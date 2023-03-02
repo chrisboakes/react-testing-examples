2df54770c123a55c232eb16f23efb4f9f9d73ce9
Chris Boakes <chris@chrisboakes.com>
2023-03-02 12:38:59 +0000

    chore: fix test

--- src/act-warning/ActWarning.test.jsx
+++ src/act-warning/ActWarning.test.jsx
@@ -1,28 +1,30 @@
-import { waitFor } from "@testing-library/react";
-import { render, screen } from "@testing-library/react";
-import userEvent from "@testing-library/user-event";
-import { ActWarning } from "./ActWarning";
+import { waitFor } from '@testing-library/react';
+import { render, screen } from '@testing-library/react';
+import userEvent from '@testing-library/user-event';
+import { ActWarning } from './ActWarning';
 
 const clipboardWriteMock = vi
-  .fn()
-  .mockImplementation((v) => Promise.resolve(v));
+	.fn()
+	.mockImplementation((v) => Promise.resolve(v));
 
 beforeEach(() => {
-  Object.defineProperty(window.navigator, "clipboard", {
-    value: { writeText: clipboardWriteMock },
-    configurable: true,
-  });
+	Object.defineProperty(window.navigator, 'clipboard', {
+		value: { writeText: clipboardWriteMock },
+		configurable: true,
+	});
 });
 
-test("copies text", async () => {
-  render(<ActWarning />);
+test('copies text', async () => {
+	render(<ActWarning />);
 
-  const button = screen.getByRole("button", {
-    name: "Copy",
-  });
+	const button = screen.getByRole('button', {
+		name: 'Copy',
+	});
 
-  await userEvent.type(screen.getByLabelText("Type and copy text"), "foo bar");
-  await userEvent.click(button);
+	await userEvent.type(screen.getByLabelText('Type and copy text'), 'foo bar');
+	await userEvent.click(button);
 
-  expect(clipboardWriteMock).toHaveBeenCalledWith("foo bar");
+	await waitFor(() => {
+		expect(clipboardWriteMock).toHaveBeenCalledWith('foo bar');
+	});
 });
